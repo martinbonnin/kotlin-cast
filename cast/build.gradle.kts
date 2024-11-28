065a68ad8b2400e4ea822a8e884b3be55efa8ed9
Martin Bonnin <martin@mbonnin.net>
2024-11-29 00:17:41 +0100

    add Kdoc and all KMP targets

--- cast/build.gradle.kts
+++ cast/build.gradle.kts
@@ -1,4 +1,7 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
 import com.gradleup.librarian.gradle.Librarian
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
   id("org.jetbrains.kotlin.multiplatform")
@@ -6,6 +9,29 @@ plugins {
 
 kotlin {
   jvm()
+  js {
+    nodejs()
+  }
+  wasmJs {
+    nodejs()
+  }
+  wasmWasi {
+    nodejs()
+  }
+  iosArm64()
+  iosSimulatorArm64()
+  iosX64()
+  macosArm64()
+  macosX64()
+  watchosArm32()
+  watchosArm64()
+  watchosDeviceArm64()
+  tvosArm64()
+  tvosSimulatorArm64()
+  tvosX64()
+  linuxX64()
+  linuxArm64()
+  mingwX64()
 
   sourceSets.getByName("commonTest") {
     dependencies {
--- cast/src/commonMain/kotlin/cast/cast.kt
+++ cast/src/commonMain/kotlin/cast/cast.kt
@@ -1,5 +1,24 @@
 package cast
 
+/**
+ * Tries to cast [this] to [T] or returns `null` if [this] is not an instance of [T].
+ *
+ * This is the equivalent of:
+ * ```
+ * this as? T
+ * ```
+ */
 inline fun <reified T : Any> Any?.safeAs(): T? = this as? T
+/**
+ * Tries to cast [this] to [T] or throws if [this] is not an instance of [T].
+ *
+ * This is the equivalent of:
+ * ```
+ * this as T
+ * ```
+ */
 inline fun <reified T : Any> Any?.cast(): T = this as T
+/**
+ * Tries to cast [this] to [T] or throws an [AssertionError] if [this] is not an instance of [T].
+ */
 inline fun <reified T : Any> Any?.assertedCast(message: () -> String): T = this as? T ?: throw AssertionError(message())
